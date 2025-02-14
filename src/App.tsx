51ffbfbb7e7c2b17128cbdda0d7ae12290bef96d
shirinshahan <shirinshahana.ae@gmail.com>
2025-02-14 12:36:05 +0400

    React-tailwind basic setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Dashboard';
+function App() {
+  return (
+    <main className="min-h-screen bg-background">
+      <Card/>
+    </main>
+  );
+}
+
+export default App;
